d0ddb82eb42e9ee36e2c131c70eb6e35188bebf9
InfiniteSwerve <jackofthestrand@gmail.com>
2023-12-13 13:15:55 -0800

    testing endpoint communication

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from openai import OpenAI
 import os
 import time
 
@@ -10,42 +9,15 @@ CORS(
     app
 )  # Enables CORS for all domains. For specific domain, use CORS(app, resources={r"/api/*": {"origins": "https://your-squarespace-site.com"}})
 
-# OPENAI_API_KEY = "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"
-KOYUKI_API_KEY = "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"
-# UWU_ID = 'asst_wxtdsDIEW8o8Z7s2DVnSRZwE'
-# CONTRARIAN_ID = "asst_Iff45DG5TaSjLPrOzSK6ztyg"
-DNS_ID = "asst_zdQvBwauDYXhh5BNG2HsktYe"
-
-client = OpenAI(api_key=KOYUKI_API_KEY)
-
 
 # TODO: Error handling
 # TODO: User input santitization
 # TODO: Better styling on the website chat
 # TODOLATER: Subdomains on squarespace
 @app.route("/chat", methods=["POST"])
-@cross_origin(
-    origins="https://lettuce-elk-tpmh.squarespace.com"
-)  # Allows CORS for this route. For specific domain, use @cross_origin(origins="https://your-squarespace-site.com")
 def chat():
     user_message = request.json["message"]
-    thread = client.beta.threads.create()
-    message = client.beta.threads.messages.create(
-        thread.id,
-        role="user",
-        content=user_message,
-    )
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=DNS_ID,
-    )
-    while run.status != "completed":
-        time.sleep(0.5)
-        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-    messages = client.beta.threads.messages.list(thread_id=thread.id)
-    print(dir(messages.data))
-    last_message = messages.data[0].content[0].text.value
-    return jsonify({"response": last_message})
+    return jsonify({"response": "request received"})
 
 
 @app.route("/")
